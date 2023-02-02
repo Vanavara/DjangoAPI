ca53f5842f855b5c9eabdc30b738a4e8654a0624
Slava <vanavara06908@gmail.com>
2023-02-02 07:51:30 +0300

    added comment for checking gitHub connection

--- djangoapi/app/serializers.py
+++ djangoapi/app/serializers.py
@@ -5,3 +5,5 @@ class AppSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = App
         fields = ('id', 'name', 'url', 'language', 'price')
+
+#comment for checking gitHub connection
